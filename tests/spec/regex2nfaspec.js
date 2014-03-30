42346f2e9a98c93807f76f74577fc391464f30af
Arnelle Balane <arnellebalane@gmail.com>
2014-03-31 07:16:21 +0800

    updated tests to new release

--- tests/spec/regex2nfaspec.js
+++ tests/spec/regex2nfaspec.js
@@ -2,7 +2,7 @@ describe('RegexParser', function() {
   it('parses empty regex', function() {
     var parsed = RegexParser.parse('');
     var nfa = new NFA('ab');
-    nfa.startState().finalize();
+    nfa.getStartState().finalize();
 
     expect(parsed).toEqual(nfa);
   });
@@ -10,7 +10,7 @@ describe('RegexParser', function() {
   it('parses abba', function() {
     var parsed = RegexParser.parse('abba');
     var nfa = new NFA('ab');
-    var q0 = nfa.startState();
+    var q0 = nfa.getStartState();
     var q1 = nfa.addState();
     var q2 = nfa.addState();
     var q3 = nfa.addState();
@@ -18,17 +18,15 @@ describe('RegexParser', function() {
     var q5 = nfa.addState();
     var q6 = nfa.addState();
     var q7 = nfa.addState();
-    var q8 = nfa.addState();
 
-    q0.transition(q1, '~');
-    q1.transition(q2, 'a');
-    q2.transition(q3, '~');
-    q3.transition(q4, 'b');
-    q4.transition(q5, '~');
-    q5.transition(q6, 'b');
-    q6.transition(q7, '~');
-    q7.transition(q8, 'a');
-    q8.finalize();
+    q0.transition(q1, 'a');
+    q1.transition(q2, '~');
+    q2.transition(q3, 'b');
+    q3.transition(q4, '~');
+    q4.transition(q5, 'b');
+    q5.transition(q6, '~');
+    q6.transition(q7, 'a');
+    q7.finalize();
 
     expect(parsed).toEqual(nfa);
   });
@@ -36,15 +34,15 @@ describe('RegexParser', function() {
   it('parses a*b', function() {
     var parsed = RegexParser.parse('a*b');
     var nfa = new NFA('ab');
-    var q0 = nfa.startState();
+    var q0 = nfa.getStartState();
     var q1 = nfa.addState();
     var q2 = nfa.addState();
     var q3 = nfa.addState();
     var q4 = nfa.addState();
 
-    q0.transition(q1, '~');
-    q1.transition(q2, 'a').transition(q2, '~');
-    q2.transition(q1, '~').transition(q3, '~');
+    q0.transition(q1, '~').transition(q1, '~');
+    q1.transition(q2, 'a');
+    q2.transition(q0, '~').transition(q3, '~');
     q3.transition(q4, 'b');
     q4.finalize();
   });
@@ -52,7 +50,7 @@ describe('RegexParser', function() {
   it('parses a+b', function() {
     var parsed = RegexParser.parse('a+b');
     var nfa = new NFA('ab');
-    var q0 = nfa.startState();
+    var q0 = nfa.getStartState();
     var q1 = nfa.addState();
     var q2 = nfa.addState();
     var q3 = nfa.addState();
@@ -70,21 +68,19 @@ describe('RegexParser', function() {
   it('parses (a+b)*', function() {
     var parsed = RegexParser.parse('(a+b)*');
     var nfa = new NFA('ab');
-    var q0 = nfa.startState();
+    var q0 = nfa.getStartState();
     var q1 = nfa.addState();
     var q2 = nfa.addState();
     var q3 = nfa.addState();
     var q4 = nfa.addState();
     var q5 = nfa.addState();
-    var q6 = nfa.addState();
 
-    q0.transition(q1, '~');
-    q1.transition(q2, '~').transition(q4, '~').transition(q6, '~');
+    q0.transition(q1, '~').finalize();
+    q1.transition(q2, '~').transition(q4, '~');
     q2.transition(q3, 'a');
-    q3.transition(q6, '~');
+    q3.transition(q0, '~').finalize();
     q4.transition(q5, 'b');
-    q5.transition(q6, '~');
-    q6.transition(q1, '~').finalize();
+    q5.transition(q0, '~').finalize();
 
     expect(parsed).toEqual(nfa);
   });
