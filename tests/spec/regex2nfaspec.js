add0bdb452a9e4877858c3026a6c53f50470cbda
Arnelle Balane <arnellebalane@gmail.com>
2014-03-31 16:12:51 +0800

    fixed test specs to adapt to new parser version

--- tests/spec/regex2nfaspec.js
+++ tests/spec/regex2nfaspec.js
@@ -38,13 +38,13 @@ describe('RegexParser', function() {
     var q1 = nfa.addState();
     var q2 = nfa.addState();
     var q3 = nfa.addState();
-    var q4 = nfa.addState();
 
-    q0.transition(q1, '~').transition(q1, '~');
-    q1.transition(q2, 'a');
-    q2.transition(q0, '~').transition(q3, '~');
-    q3.transition(q4, 'b');
-    q4.finalize();
+    q0.transition(q1, 'a').transition(q2, '~');
+    q1.transition(q0, '~').transition(q2, '~');
+    q2.transition(q3, 'b');
+    q3.finalize();
+
+    expect(parsed).toEqual(nfa);
   });
 
   it('parses a+b', function() {
@@ -73,14 +73,12 @@ describe('RegexParser', function() {
     var q2 = nfa.addState();
     var q3 = nfa.addState();
     var q4 = nfa.addState();
-    var q5 = nfa.addState();
 
-    q0.transition(q1, '~').finalize();
-    q1.transition(q2, '~').transition(q4, '~');
-    q2.transition(q3, 'a');
-    q3.transition(q0, '~').finalize();
-    q4.transition(q5, 'b');
-    q5.transition(q0, '~').finalize();
+    q0.transition(q1, '~').transition(q3, '~').finalize();
+    q1.transition(q2, 'a');
+    q2.transition(q0, '~').finalize();
+    q3.transition(q4, 'b');
+    q4.transition(q0, '~').finalize();
 
     expect(parsed).toEqual(nfa);
   });
